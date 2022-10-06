bab154d7f961accc7355faae400fa739541902da
Nic <nic.dan.costea@gmail.com>
2022-10-06 09:39:40 +0200

    Adding initial files
    - tested linkage between files [OK]

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const student = {
+  firstName: "Gordo",
+  lastName: "Ram",
+  nickName: "Gogo",
+  age: 134,
+  isWise: true,
+};
+
+const fullName = `${student.firstName} '${student.nickName}' ${student.lastName}`;
+console.log({ fullName });
