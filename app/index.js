3bbd551ae906cc28438fb9b5c426622a67d8d619
Nic <nic.dan.costea@gmail.com>
2022-10-06 11:54:14 +0200

    Checkpoint
    - want to save all the progress

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const student = {
   age: 134,
   isWise: true,
   "batch name": "asparagus",
-  "dynamic content": false,
+  "dynamic content": true,
 };
 
 // READING & Using the properties of the object
@@ -40,3 +40,71 @@ console.log(
   "Using [] notation we can do computations before accessing the property ",
   student[dynamic + " content"]
 );
+
+// CHANGE a property in an object
+student["batch name"] = "donkey";
+console.log({ student });
+
+// ADD PROPERTIES CONDITIONALLY
+// we want to be able to change the  "hasDrivingLicence" property which is initially set to false
+// +
+// we want to create a new property "hasScooterLicence" based on the age
+student.hasDrivingLicence = false;
+student.age = 13;
+const newProperty =
+  student.age > 18 ? "hasDrivingLicence" : "hasScooterLicence";
+student[newProperty] = true;
+console.log({ student });
+
+// ANOTHER EXAMPLE - when someone inputs the letter she wants an example for, form the dictionary
+const dictionary = {
+  a: "alumni",
+  b: "banana",
+  c: "cokonut",
+};
+let favoriteLetter = "c";
+const searchedLetter = dictionary[favoriteLetter];
+if (searchedLetter !== undefined && typeof searchedLetter !== "number") {
+  console.log(`This is the whole ${dictionary}`);
+  console.log(
+    `The example for the ${favoriteLetter} letter you inputted is: ${searchedLetter}`
+  );
+} else {
+  console.log(
+    "The letter you're searching is NOT yet in the dicitonary OR has not yet been defined OR is not a string"
+  );
+}
+
+// CLONING OBJECTS:
+// 02h:30min - onwards in the video
+// The WRONG WAY because objects go by REFERENCE:
+const student2 = student;
+console.log({ student2 });
+// if we try to change the location in student2, the location
+student2.location = "Mars";
+console.log({ student });
+// The CORRECT WAY using Object.assign
+const student3 = Object.assign({}, student);
+console.log({ student3 });
+
+// NESTING AN OBJECT INSIDE ANOTHER OBJECT
+student3.address = {
+  street: "Rahova No.1",
+  city: "Roma",
+};
+console.log({ student3 });
+
+// CLONING NESTED OBJECTS WITHIN OBJECTS
+const student4 = Object.assign({}, student3);
+console.log({ student4 });
+// we want to create a new address object in student4 in order to be able to edit it
+student4.address = {};
+console.log({ student4 });
+// we clone only the address object from student3 to student4
+Object.assign(student4.address, student3.address);
+console.log({ student4 });
+// we want to change the address object in student4 without affecting student3
+student4.address.street = "Malamu No.3";
+student4.address.city = "Macao";
+console.log({ student3 });
+console.log({ student4 });
